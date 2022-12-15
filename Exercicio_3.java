120f133f2d5327724e71258c52fb02ba9c8d3132
Victor Perez <110212717+Victor2Perez@users.noreply.github.com>
2022-12-15 17:43:49 -0300

    Add files via upload

---
+++ Exercicio_3.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Exercicio_3 {
+    public static void main (String[] args) {
+        Scanner ler = new Scanner(System.in);
+
+        int i;
+
+        for(i=10; i>1; i--) {
+            System.out.println(i);
+        }
+
+    } 
+} 
